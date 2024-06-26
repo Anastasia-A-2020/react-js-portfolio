52be33c8d5375173ca1673cd43384015347efba0
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2024-06-26 17:20:06 +0300

    navbar & my skills sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Homescreen";
+import Navbar from "./Pages/Home/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
